c1f2d82a7964369e8a89fb7d21b88ab7d0615a75
Robertspoon <robertthemcjug@gmail.com>
2023-01-25 22:07:12 -0400

    Started teleport/killbox/checkpoints

---
+++ Assets/scripts/CheckPointSystem.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPointSystem : MonoBehaviour
+{
+    public Vector3 spawnPosition;
+    //public GameObject checkPoint;
+
+
+
+
+    void Start()
+    {
+        spawnPosition = gameObject.transform.position;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Killbox"))
+        {
+            Vector3 deltaPosition = spawnPosition - transform.position;
+
+            gameObject.GetComponent<CharacterController>().Move(deltaPosition);
+            gameObject.GetComponent<CharacterController>().SimpleMove(Vector3.zero);
+        }
+
+        //if (other.gameObject.CompareTag("Checkpoint"))
+       // {
+        //    spawnPosition = checkPoint.transform.position;
+       //     Destroy(checkPoint);
+      //  }
+
+
+
+    }
+}
